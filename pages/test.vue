92138945126e8e543de44c212233ad03c1feeebc
Alex Patin <alex@mona.gallery>
2022-04-16 16:18:45 -0400

    Added a test page

---
+++ pages/test.vue
@@ -0,0 +1,33 @@
+<script>
+import AvatarConnect from '@avatarconnect/sdk'
+
+import { getBaseUrl } from '~/utils/helpers'
+
+/**
+ * INSTRUCTIONS:
+ * -----------------------------------
+ * Modify this provider config array to include
+ * your provider ID and/or configuration tuple
+ */
+const PROVIDER_CONFIG = []
+
+export default {
+  name: 'Test',
+  mounted() {
+    if (!process.browser) return
+
+    this.connector = new AvatarConnect(PROVIDER_CONFIG, {
+      bridgeUrl: getBaseUrl(window.location.href),
+      maxHeight: 610,
+    })
+
+    // eslint-disable-next-line no-console
+    this.connector.on('result', console.log)
+    // eslint-disable-next-line no-console
+    this.connector.on('error', console.error)
+
+    this.connector.enable()
+  },
+  render: () => null,
+}
+</script>
